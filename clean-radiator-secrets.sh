c259babf09120c6268d16cb602be5cc61ceb9300
Jan Tomasek <jan@tomasek.cz>
2017-04-02 12:35:27 +0200

    two more strings to secure

--- clean-radiator-secrets.sh
+++ clean-radiator-secrets.sh
@@ -5,4 +5,6 @@ cat radius-orig.cfg | sed "s/Secret.*$/Secret\tXXYYXX/" |\
   sed "s/AuthDN.*$/AuthDN\tXXYYXX/" |\
   sed "s/AuthPassword.*$/AuthPassword\tXXYYXX/" |\
   sed "s/BaseDN.*$/BaseDN\tXXYYXX/" |\
-  sed "s/PasswordAttr.*$/PasswordAttr\tXXYYXX/" > radius.cfg
+  sed "s/PasswordAttr.*$/PasswordAttr\tXXYYXX/" |\
+  sed "s/CATrustTestPswd\s.*$/CATrustTestPswd\tXXYYXX/" |\
+  sed "s/CATrustTestUser\s.*$/CATrustTestUser\tXXYYXX/" > radius.cfg
